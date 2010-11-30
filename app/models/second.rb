ecbd974433dc556590330f8f4b86e0d01d5e61c4
Scott Gonyea <me@aitrus.org>
2010-11-29 16:29:29 -0800

    Method comment

--- app/models/second.rb
+++ app/models/second.rb
@@ -5,6 +5,7 @@ class Second < Event
                           }
 
 private
+  # Will error if the motion creator attempts to second their motion
   def motion_creator_cannot_second
     errors.add(:member, "Member cannot second a motion that they created.") if motion.member == member
   end
