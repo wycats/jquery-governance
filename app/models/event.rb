34e49543dff292c6788864333938fc8dbc2984c3
Scott Gonyea <me@aitrus.org>
2010-11-30 15:53:25 -0800

    Fixed my scope stupidity

--- app/models/event.rb
+++ app/models/event.rb
@@ -15,8 +15,8 @@ class Event < ActiveRecord::Base
   validate    :motion_creator_cannot_second,  :if => :is_second?
   after_save  :assert_motion_state,           :if => :is_vote?
 
-  scope :votes,   :where => {:event_type  => "vote"}
-  scope :seconds, :where => {:event_type  => "second"}
+  scope :votes,   where(:event_type  => "vote")
+  scope :seconds, where(:event_type  => "second")
 
   # @return [true, false] Whether or not this is a Voting Event
   def is_vote?
