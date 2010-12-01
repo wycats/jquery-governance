3b1a05a545d8ed4f08783f26751635cb58d15c84
David Trasbo <me@dtrasbo.com>
2010-12-01 16:19:26 +0100

    Make "Create motion" feature pass

--- features/step_definitions/member_steps.rb
+++ features/step_definitions/member_steps.rb
@@ -1,7 +1,7 @@
 Given /^I am signed in as an active member$/ do
   visit new_member_session_path
   fill_in 'Email', with: active_member.email
-  fill_in 'Password', with: active_member.password
+  fill_in 'Password', with: 'secret'
   click_button 'Sign in'
 end
 
