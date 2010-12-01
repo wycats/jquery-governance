260fd5cc5c893cffe354c92a78b5dc7cef188bc6
Scott Gonyea <me@aitrus.org>
2010-11-30 20:13:48 -0800

    Trailing Whitespace

--- app/models/motions/waitingexpedited_to_failed.rb
+++ app/models/motions/waitingexpedited_to_failed.rb
@@ -1,9 +1,9 @@
 class Motions::WaitingexpeditedToFailed
   @queue = :waitingexpedited_to_failed
- 
+
   def self.perform(motion_id)
     motion = Motion.find(motion_id)
-   
+
     if motion.waitingexpedited?
       motion.failed!
     end
