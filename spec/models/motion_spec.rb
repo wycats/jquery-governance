2a1561bcb52788932eecb423d6448e70a04145bf
thedelchop <joseph.delcioppio@gmail.com>
2010-12-04 17:01:29 -0500

    Refactor Event spec for more coverage and performance.  Scope Event#yeas and Event#nays to Event#votes, since seconds don't share that property.

--- spec/models/motion_spec.rb
+++ spec/models/motion_spec.rb
@@ -3,27 +3,28 @@ require 'spec_helper'
 describe Motion do
   before do
     @motion = Factory.create(:motion)
+    @member = Factory.create(:active_membership).member
   end
 
   it "knows how many yea votes have been cast for the motion" do
-    4.times { Factory.create(:yes_vote, :motion => @motion) }
-    @motion.yeas.should == 4
+    2.times { Factory.create(:yes_vote, :motion => @motion) }
+    @motion.yeas.should == 2
   end
 
   it "knows how many nea votes have been cast for the motion" do
-    3.times { Factory.create(:no_vote, :motion => @motion) }
-    @motion.nays.should == 3
+    1.times { Factory.create(:no_vote, :motion => @motion) }
+    @motion.nays.should == 1
   end
 
   describe 'voting requirements' do
     before do
       #This reprsents all of the members who currently have the right to vote
-      8.times { Factory.create(:active_membership) }
+      4.times { Factory.create(:active_membership) }
     end
 
     describe 'required_votes' do
       it "knows how many votes are required to pass the motion" do
-         @motion.required_votes.should == 5
+         @motion.required_votes.should == 3
       end
     end
 
@@ -31,7 +32,7 @@ describe Motion do
 
       describe "when there more than half of the possible votes are yeas" do
         before do
-          5.times { Factory.create(:yes_vote, :motion => @motion) }
+          3.times { Factory.create(:yes_vote, :motion => @motion) }
         end
 
         it "knows the requirment for passage has been met" do
@@ -41,7 +42,7 @@ describe Motion do
 
       describe "when exactly half of the possible votes are yeas" do
         before do
-          4.times{  Factory.create(:yes_vote, :motion => @motion)}
+          2.times{  Factory.create(:yes_vote, :motion => @motion)}
         end
 
         it "knows that the requirement for passage hasn't been met" do
@@ -51,7 +52,7 @@ describe Motion do
 
       describe "when less than half the possible votes are yeas" do
         before do
-          3.times{  Factory.create(:yes_vote, :motion => @motion)}
+          1.times{  Factory.create(:yes_vote, :motion => @motion)}
         end
 
         it "knows that the requirement for passage hasn't been met" do
@@ -62,13 +63,12 @@ describe Motion do
 
     describe 'seconds_for_expedition' do
       it "knows how many seconds are needed to expidite the measure" do
-        @motion.seconds_for_expedition.should == 3
+        @motion.seconds_for_expedition.should == 2
       end
     end
 
     describe "vote" do
       it "creates a new vote with the given member and value" do
-        @member = Factory.create(:active_membership).member
         @motion.vote(@member, true)
         Event.votes.last.member.should eql @member
         Event.votes.last.value.should be_true
@@ -76,10 +76,6 @@ describe Motion do
     end
 
     describe 'second(member)' do
-      before do
-        @member = Factory.create(:member)
-      end
-
       it "creates a new second for the member" do
         lambda do
           @motion.second(@member)
@@ -91,7 +87,6 @@ describe Motion do
   describe 'conflicts_with_member?' do
     before :each do
       @conflict = Factory(:conflict)
-      @member = Factory.create(:member)
     end
 
     describe "when a member has a conflict unrelated to this motion" do
