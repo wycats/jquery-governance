00df95d8b2141b7dad67c1e41f44f49814d706b3
Gabriel Andretta <ohhgabriel@gmail.com>
2010-12-05 18:16:16 -0300

    clean some comments
    We don't need them anymore since the code is already telling that :)

--- app/models/motion.rb
+++ app/models/motion.rb
@@ -112,10 +112,6 @@ class Motion < ActiveRecord::Base
 
   # @TODO - Description
   def waitingsecond!
-    # enqueue a job for 48 hours
-    #
-    # - if in the waitingsecond state, go into the failed state
-    # - otherwise, do nothing
     update_state("waitingsecond")
   end
 
@@ -125,16 +121,6 @@ class Motion < ActiveRecord::Base
 
   # @TODO - Description
   def waitingexpedited!
-    # enqueue a job for 24 hours
-    #
-    # - if in the waitingexpedited state and there are 2
-    #   seconds, go into the waitingobjection state
-    # - otherwise, do nothing
-    #
-    # enqueue a job for 48 hours
-    #
-    # - if in the waitingexpedited state, go to the failed tate
-    # - otherwise, do nothing
     self.expedited = true
     update_state("waitingsecond")
   end
@@ -145,11 +131,6 @@ class Motion < ActiveRecord::Base
 
   # @TODO - Description
   def waitingobjection!
-    # enqueue a job for 24 hours from now.
-    #
-    # - if in the waitingobjection state, go to the voting state
-    # - if in the objected state, enqueue a job for 24 hours
-    #   - at that time, go to the voting state
     update_state("discussing")
   end
 
@@ -170,11 +151,6 @@ class Motion < ActiveRecord::Base
 
   # @TODO - Description
   def voting!
-    # enqueue a job for 48 hours for now
-    #
-    # - if a majority of active members did not vote yes,
-    #   go to the failed state
-    # - otherwise, go into the closed state
     update_state("voting")
   end
 
