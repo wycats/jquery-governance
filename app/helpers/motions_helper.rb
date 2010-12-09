e539598f29ee69c982fa0c26f86acb8938304a21
Gabriel Andretta <ohhgabriel@gmail.com>
2010-12-08 23:54:18 -0300

    stop cycling colors when listing motions

--- app/helpers/motions_helper.rb
+++ app/helpers/motions_helper.rb
@@ -1,2 +1,16 @@
 module MotionsHelper
+  def html_attrs_for_motion(motion)
+    {
+      :id    => dom_id(motion),
+      :class => "#{dom_class(motion)} #{state_class_for_motion(motion)}".strip
+    }
+  end
+
+  def state_class_for_motion(motion)
+    if motion.has_met_requirement?
+      'passed'
+    elsif motion.failed?
+      'failed'
+    end
+  end
 end
