862d39f0edd0aec5f6ac29100a68e6dc65373243
Gabriel Andretta <ohhgabriel@gmail.com>
2010-12-16 20:06:01 -0300

    fix whitespace problems

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,12 +1,12 @@
 class Admin::BaseController < ApplicationController
   before_filter :authenticate_member!
   before_filter :admin_required
-  
+
   respond_to :html
 
-  
+
   private
-  
+
   def admin_required
     current_member.is_admin?
   end
--- spec/controllers/admin/base_controller_spec.rb
+++ spec/controllers/admin/base_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Admin::BaseController do
-  
+
   describe "#index with admin logged in" do
     before(:each) do
       @admin = Factory(:member, :is_admin => true)
@@ -9,11 +9,11 @@ describe Admin::BaseController do
       controller.stub!(:current_member).and_return @admin
       controller.stub!(:admin_required).and_return true
     end
-    
+
     it "should rendere the admin index template" do
       get :index
       response.should render_template('admin/base/index')
     end
   end
-  
+
 end
