61197808e48c3fdab859d063476d08cf07c8287e
thedelchop <joseph.delcioppio@gmail.com>
2010-12-19 16:53:21 -0500

    Add admin control of a member's memberships. Add active_memberships_controller, index view and add link to edit a member's memberships if the admin is view members#edit.

--- app/controllers/admin/active_memberships_controller.rb
+++ app/controllers/admin/active_memberships_controller.rb
@@ -1,17 +1,12 @@
 class Admin::ActiveMembershipsController < Admin::BaseController
   before_filter :load_member
   def index
-    @active_memberships = @member.active_memberships
-    @expired_memberhips = @member.active_memberships.expired
+    @active_membership = @member.active_at?(Time.now)
+    @expired_memberships = @member.active_memberships.expired
   end
-
-  def edit
-    @active_membership = @member.active_memberships.where(:id => params[:id])
-  end
-
 
   private
-  
+
   def load_member
     @member = Member.find(params[:member_id])
   end
