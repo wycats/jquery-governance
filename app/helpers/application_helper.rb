90c0b9bf0ab00866a20eff3031b1743a6207c658
Gabriel Andretta <ohhgabriel@gmail.com>
2010-12-25 18:00:10 -0300

    bug fix and dry application helpers up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,20 +13,22 @@ module ApplicationHelper
   end
 
   def positive_button_to(name, options = {}, html_options = {})
-    html_options[:class] ||= ''
-    html_options[:class] << 'button positive'
+    append_html_classes(html_options, 'button', 'possitive')
     button_to(name, options, html_options)
   end
 
   def negative_button_to(name, options = {}, html_options = {})
-    html_options[:class] ||= ''
-    html_options[:class] << 'button negative'
+    append_html_classes(html_options, 'button', 'negative')
     button_to(name, options, html_options)
   end
 
   def badge(text, html_options={})
-    html_options[:class] ||= ''
-    html_options[:class] << ' badge'
+    append_html_class(html_options, 'badge')
     content_tag(:span, html_options) { text }
   end
+
+  def append_html_classes(html_options, *classes)
+    html_options[:class] = [html_options[:class], *classes].compact.join(' ')
+  end
+  alias :append_html_class :append_html_classes
 end
