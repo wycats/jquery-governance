8919b10d283a9a3934a981c93882a3f3177ca868
Gabriel Andretta <ohhgabriel@gmail.com>
2010-12-26 23:34:39 -0300

    add docs to app/models/motion_state/discussing.rb

--- app/models/motion_state/discussing.rb
+++ app/models/motion_state/discussing.rb
@@ -1,17 +1,30 @@
 module MotionState
+
+  # Handles the responsabilities specific of a {Motion motion} in the
+  # discussing state.
+  #
+  # A discussing motion can only be seen and be objected by active members, no
+  # one can second or vote on it.
   class Discussing < Base
     include NoVotable
     include ActiveMemberViewable
     include NoSecondable
 
+    # Indicates whether or not a given member is allowed to object a motion.
+    # @param [Member] member A member who wants to object the motion.
+    # @return [true, false] Only active members can object a motion and they can do it only once.
     def permit_object?(member)
       member.membership_active? && @motion.objections.where(:member_id => member.id).blank?
     end
 
+    # Schedule automatic updates in 24 and 48 hours.
+    # @see Base#schedule_updates_in
     def schedule_updates
       schedule_updates_in(24.hours, 48.hours)
     end
 
+    # Updates the state of a motion to voting when 24 hours has been passed and
+    # the motion hasn't been objected, or when 48 hours has been passed.
     def scheduled_update(time_elapsed)
       if time_elapsed >= 48.hours
         @motion.voting!
