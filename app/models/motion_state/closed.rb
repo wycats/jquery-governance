0f620900f62a4c1bc19417b88b5f674dd1b3f773
Gabriel Andretta <ohhgabriel@gmail.com>
2010-12-26 23:24:32 -0300

    add docs to app/models/motion_state/closed.rb

--- app/models/motion_state/closed.rb
+++ app/models/motion_state/closed.rb
@@ -1,4 +1,10 @@
 module MotionState
+
+  # Handles the responsabilities specific of a {Motion motion} in the closed
+  # state.
+  #
+  # A closed motion can be seen by everyone, but no one can second, object or
+  # vote on it.
   class Closed < Base
     include PubliclyViewable
     include NoSecondable
