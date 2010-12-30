13d57132e9880e8eea45ac36a43e76caa707a3c3
thedelchop <joseph.delcioppio@gmail.com>
2010-12-30 17:24:47 -0500

    Cleanup whitespace in these files

--- spec/support/rspec_ext.rb
+++ spec/support/rspec_ext.rb
@@ -1,7 +1,7 @@
 # JD and YK (but mostly YK :) ): Rails master has changed the way that layouts
 # are passed to the renderer.  Instead of strings they are now passed as arrays
 # so that view can also render the layouts of their parents.  Rspec hasn't been
-# updated yet it seems so this patch will get us through until its updated. 
+# updated yet it seems so this patch will get us through until its updated.
 
 module RSpec
   module Rails
