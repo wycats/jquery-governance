2b929aeae1f3e55f4278464b1dbef3f32a0d425d
Gabriel Andretta <ohhgabriel@gmail.com>
2011-01-06 20:49:00 -0300

    remove duplication

--- app/helpers/motions_helper.rb
+++ app/helpers/motions_helper.rb
@@ -6,28 +6,8 @@ module MotionsHelper
     classes.join(' ')
   end
 
-  def render_motion_group(name, motion_group)
-    content_tag(:section) do
-      content = content_tag(:h2, t("motions.#{name}.heading"))
-      if motion_group.empty?
-        content << content_tag(:div, t("motions.#{name}.empty"), :class => 'empty')
-      else
-        content << content_tag(:ul, render_motions_list_items(motion_group), :class => 'motions')
-      end
-      content.html_safe
-    end
-  end
-
   def render_motions_list_items(motions)
-    motions.map do |motion|
-      render(
-        :partial => 'motions/list_item',
-        :locals  => {
-          :motion => motion,
-          :last   => motion == motions.last && motions.size >= motions.count
-        }
-      )
-    end.join.html_safe
+    render :partial => 'motions/list_item', :collection => motions, :as => :motion
   end
 
   def motion_status_badge(motion)
