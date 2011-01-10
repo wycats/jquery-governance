92524bc23c04ae354786e7967a4fa18a04382d14
thedelchop <joseph.delcioppio@gmail.com>
2011-01-09 19:21:38 -0500

    Remove member_conflict and motion_conflict.  A polymorphic approach to conflicts is a cleaner more manageable way to do this.

--- app/models/member_conflict.rb
+++
@@ -1,4 +0,0 @@
-class MemberConflict < ActiveRecord::Base
-  belongs_to :conflict
-  belongs_to :member
-end
--- app/models/motion_conflict.rb
+++
@@ -1,4 +0,0 @@
-class MotionConflict < ActiveRecord::Base
-  belongs_to :conflict
-  belongs_to :motion
-end
