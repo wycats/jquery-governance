f527f28339c47b4ae6472021e39d6b59c41a9234
thedelchop <joseph.delcioppio@gmail.com>
2011-01-09 20:17:54 -0500

    Add some basic specs for a conflict

--- app/models/conflict.rb
+++ app/models/conflict.rb
@@ -1,2 +1,12 @@
 class Conflict < ActiveRecord::Base
+
+  validates :name, :presence => true
+
+  def ==(object)
+    super || (object.is_a?(Conflict) && name == object.name)
+  end
+
+  def to_s
+    name
+  end
 end
--- spec/models/conflict_spec.rb
+++ spec/models/conflict_spec.rb
@@ -5,17 +5,19 @@ describe Conflict do
     @conflict = Factory.create(:conflict)
   end
 
-  it "should have relations with motions" do
-    motion = Factory.create(:motion)
-    motion.conflicts << @conflict
-    motion.conflicts.size.should == 1
-    motion.conflicts.first.title.should == @conflict.title
+  it "should require a name" do
+    @conflict.name = nil
+    @conflict.should_not be_valid
   end
 
-  it "should have relations with members" do
-    member = Factory.create(:john_resig)
-    member.conflicts << @conflict
-    member.conflicts.size.should == 1
-    member.conflicts.first.title.should == @conflict.title
+  it "should equal a conflict with the same name" do
+    @conflict.name = "awesome"
+    @new_conflict = Factory.create(:conflict, :name => "awesome")
+    @new_conflict.should == @conflict
+  end
+
+  it "should return its name when to_s is called" do
+    @conflict.name = "cool"
+    @conflict.to_s.should == "cool"
   end
 end
