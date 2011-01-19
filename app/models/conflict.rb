f9dc20a86636a0e4a403172e512300688dc0bd2c
thedelchop <joseph.delcioppio@gmail.com>
2011-01-19 08:54:45 -0500

    Finish conflict and its spec.  Most of the methods of conflict focus on not creating conflicts with the same names, but instead returning the already existing conflict if one exists.

--- app/models/conflict.rb
+++ app/models/conflict.rb
@@ -1,4 +1,5 @@
 class Conflict < ActiveRecord::Base
+  has_many :conflictions
 
   validates :name, :presence => true, :uniqueness => true
 
@@ -16,6 +17,22 @@ class Conflict < ActiveRecord::Base
 
   def self.named_like_any(list)
     where(list.map { |conflict| sanitize_sql(["name ILIKE ?", "%#{conflict.to_s}%"]) }.join(" OR "))
+  end
+
+  def self.find_or_create_with_named_like(name)
+    named_like(name).first || create(:name => name)
+  end
+
+  def self.find_or_create_all_with_like_by_name(*list)
+    list = [list].flatten
+
+    return [] if list.empty?
+
+    existing_conflicts = Conflict.named_any(list).all
+    new_conflict_names = list.reject { |name| existing_conflicts.any? { |conflict| conflict.name.mb_chars.downcase == name.mb_chars.downcase } }
+    created_conflicts  = new_conflict_names.map { |name| Conflict.create(:name => name) }
+
+    existing_conflicts + created_conflicts
   end
 
   def ==(object)
--- spec/models/conflict_spec.rb
+++ spec/models/conflict_spec.rb
@@ -2,7 +2,13 @@ require 'spec_helper'
 
 describe Conflict do
   before do
-    @conflict = Factory.create(:conflict)
+    @conflict = Factory.create(:conflict, :name => "conflict")
+    @lower_case_conflict = Factory.create(:conflict, :name => "test")
+    @upper_case_conflict = Factory.create(:conflict, :name => "TEST")
+    @mixed_case_conflict = Factory.create(:conflict, :name => "tEsT")
+    @partial_match = Factory.create(:conflict, :name => "testing")
+    @lower_case_second_conflict = Factory.create(:conflict, :name => "exam")
+    @long_form_second_conflict = Factory.create(:conflict, :name => "examination")
   end
 
   it "should require a name" do
@@ -17,12 +23,6 @@ describe Conflict do
 
   context "name scopes" do
     before :all do
-      @lower_case_conflict = Factory.create(:conflict, :name => "test")
-      @upper_case_conflict = Factory.create(:conflict, :name => "TEST")
-      @mixed_case_conflict = Factory.create(:conflict, :name => "tEsT")
-      @partial_match = Factory.create(:conflict, :name => "testing")
-      @lower_case_second_conflict = Factory.create(:conflict, :name => "exam")
-      @long_form_second_conflict = Factory.create(:conflict, :name => "examination")
     end
 
     describe "self.named" do
@@ -43,13 +43,61 @@ describe Conflict do
 
     describe "self.named_any" do
       it "should return any conflict that is a case-insensitive match to the list" do
-        Conflict.named_any(%w[test exam]).should include(@lower_case_conflict, @upper_case_conflict, @mixed_case_conflict, @lower_case_second_conflict)      
+        Conflict.named_any(%w[test exam]).should include(@lower_case_conflict, @upper_case_conflict, @mixed_case_conflict, @lower_case_second_conflict)
       end
     end
 
     describe "self.named_any_like" do
       it "should return any conflict that is at least a partial match to one of the name in the list" do
-        Conflict.named_like_any(%w[test exam]).should include(@lower_case_conflict, @upper_case_conflict, @mixed_case_conflict, @partial_match, @lower_case_second_conflict, @long_form_second_conflict)   
+        Conflict.named_like_any(%w[test exam]).should include(@lower_case_conflict, @upper_case_conflict, @mixed_case_conflict, @partial_match, @lower_case_second_conflict, @long_form_second_conflict)
+      end
+    end
+  end
+
+  describe "find_or_create_with_named_like" do
+    context "when there is an conflict that already exists with that name" do
+      it "returns that already existing conflict" do
+        Conflict.find_or_create_with_named_like("conflict").should == @conflict
+      end
+
+      it "doesn't create a new conflict" do
+        lambda do
+          Conflict.find_or_create_with_named_like("conflict")
+        end.should_not change(Conflict, :count)
+      end
+    end
+
+    context "when there is no conflict that already exists with that name" do
+      it "returns a new conflict with the specified name" do
+        lambda do
+          Conflict.find_or_create_with_named_like("new conflict")
+        end.should change(Conflict, :count).by(1)
+      end
+    end
+  end
+
+  describe "find_or_create_all_with_named_like" do
+    context "when there are no names specified in the list" do
+      it "returns an empty array" do
+        Conflict.find_or_create_all_with_like_by_name().should == []
+      end
+    end
+
+    context "when there are conflicts that already exist with one of the specified names" do
+      it "returns the conflicts with the specified names" do
+        Conflict.find_or_create_all_with_like_by_name("test", "conflict").should include(@conflict, @upper_case_conflict, @lower_case_conflict, @mixed_case_conflict)
+      end
+    end
+
+    context "when one of the names specified doesn't already exist" do
+      it "returns the conflicts that already exist" do
+        Conflict.find_or_create_all_with_like_by_name("test", "conflict", "new conflict").should include(@conflict, @upper_case_conflict, @lower_case_conflict, @mixed_case_conflict)
+      end
+
+      it "creates a conflict with the specified name that doesn't already exist" do
+        lambda do
+          Conflict.find_or_create_all_with_like_by_name("test", "conflict", "new conflict")
+        end.should change(Conflict, :count).by(1)
       end
     end
   end
