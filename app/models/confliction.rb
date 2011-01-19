8e6f684962e1e90d9738388693c7e64dc8f2ae92
thedelchop <joseph.delcioppio@gmail.com>
2011-01-19 08:56:26 -0500

    A confliction is the interestion of a conflict and something that is conflictable.  At the moment, that is a Member or a Motion.  This implementation allows multipe motions and members to share the same conflict.  This will ensure in the future that when searching for Members or Motions share a conflict, that all will be found.

--- app/models/confliction.rb
+++ app/models/confliction.rb
@@ -1,4 +1,7 @@
 class Confliction < ActiveRecord::Base
   belongs_to :conflict
   belongs_to :conflictable, :polymorphic => true
+
+  validates :conflict_id, :uniqueness => {:scope => [:conflictable_type, :conflictable_id]},
+                          :presence => true
 end
--- spec/models/confliction_spec.rb
+++ spec/models/confliction_spec.rb
@@ -1,4 +1,23 @@
 require 'spec_helper'
 
 describe Confliction do
+  before(:each) do
+    @confliction = Factory.build(:member_confliction)
+  end
+
+  it "should not be valid with a invalid conflict" do
+    @confliction.conflictable = Factory.create(:member)
+    @confliction.conflict = nil
+
+    @confliction.should_not be_valid
+  end
+
+  it "should not create duplicate conflictions" do
+    @conflictable = Factory.create(:motion)
+    @conflict = Factory.create(:conflict)
+
+    lambda {
+      2.times { Confliction.create( :conflictable => @conflictable, :conflict => @conflict) }
+    }.should change(Confliction, :count).by(1)
+  end
 end
