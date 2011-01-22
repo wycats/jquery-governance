4ca9652a9eabb9875d8f4232de8edc89a5d08b17
Gabriel Andretta <ohhgabriel@gmail.com>
2011-01-22 18:09:41 -0300

    try to get Resque working on Heroku

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,8 +1,13 @@
-resque_config = YAML.load(File.open("#{File.dirname(__FILE__)}/../resque.yml").read)
-redis         = resque_config[Rails.env]
-
-if redis
-  Resque.redis = redis
+if Rails.env == 'production'
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 else
-  raise StandardError.new("Redis not defined for #{Rails.env}") unless Rails.env == "production"
+  resque_config = YAML.load(File.open("#{File.dirname(__FILE__)}/../resque.yml").read)
+  redis         = resque_config[Rails.env]
+
+  if redis
+    Resque.redis = redis
+  else
+    raise StandardError.new("Redis not defined for #{Rails.env}")
+  end
 end
