fa999362353be893d72def81f79bef2c3b61d145
Gabriel Andretta <ohhgabriel@gmail.com>
2011-01-23 15:00:41 -0300

    that shouldn't be there in the first place

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,4 @@
 module ApplicationHelper
-  # Will grab the name of the motion's creator and handle any exceptions that arise.
-  # @todo: This shouldn't really exist, but I don't want pages to blowed up while testing
-  # @param [Motion] motion The motion in question
-  # @return [String] The name of the motion's creator or "Member" if invalid
-  def motion_creator_display_name(motion)
-    begin
-      motion.member.name
-    rescue NoMethodError
-      Rails.logger.warn "Creator of Motion (#{motion.inspect}) either does not exist or does not have a name assigned"
-      "Member"
-    end
-  end
-
   def positive_button_to(name, options = {}, html_options = {})
     append_html_classes(html_options, 'button', 'positive')
     button_to(name, options, html_options)
