892655534b0267456de59d2cc801f5f4e5ea1f6e
Gabriel Andretta <ohhgabriel@gmail.com>
2011-02-02 18:02:24 -0300

    make ScheduledMotionUpdate specs suitable for refactoring

--- spec/models/scheduled_motion_update_spec.rb
+++ spec/models/scheduled_motion_update_spec.rb
@@ -1,21 +1,93 @@
 require 'spec_helper'
 
 describe ScheduledMotionUpdate do
-  before(:all) do
-    @motion = Factory(:motion)
-  end
-
   describe ".perform" do
-    it "finds the motion" do
-      @motion.stub(:scheduled_update)
-      Motion.should_receive(:find_by_id_and_state_name).with(@motion.id, @motion.state_name).and_return(@motion)
-      ScheduledMotionUpdate.perform(@motion.id, @motion.state_name, 24.hours)
+    context "for a motion waiting for seconds that hasn't been marked as expedited" do
+      before :all do
+        @motion = Factory(:motion)
+        @motion_state = @motion.state
+      end
+
+      it "doesn't update the motion state before 48 hours" do
+        ScheduledMotionUpdate.perform(@motion.id, 'waitingsecond', 47.hours)
+        @motion.reload.should be_waitingsecond
+      end
+
+      it "updates the motion state to 'closed' after 48 if it doesn't get enough seconds" do
+        ScheduledMotionUpdate.perform(@motion.id, 'waitingsecond', 48.hours)
+        @motion.reload.should be_closed
+      end
     end
 
-    it "sends the message 'scheduled_update' to the motion" do
-      Motion.stub(:find_by_id_and_state_name => @motion)
-      @motion.should_receive(:scheduled_update).with(24.hours)
-      ScheduledMotionUpdate.perform(@motion.id, @motion.state_name, 24.hours)
+    context "for a motion waiting for seconds that has been marked as expedited" do
+      before do
+        @motion = Factory(:motion, :expedited => true)
+      end
+
+      it "doesn't update the motion state before 48 hours" do
+        ScheduledMotionUpdate.perform(@motion.id, 'waitingsecond', 47.hours)
+        @motion.reload.should be_waitingsecond
+      end
+
+      it "updates the motion state to 'discussing' after 48 if it can be discussed" do
+        2.times { Factory(:second, :motion_id => @motion.id) }
+        ScheduledMotionUpdate.perform(@motion.id, 'waitingsecond', 48.hours)
+        @motion.reload.should be_discussing
+      end
+
+      it "updates the motion state to 'closed' after 48 if it can't be discussed" do
+        ScheduledMotionUpdate.perform(@motion.id, 'waitingsecond', 48.hours)
+        @motion.reload.should be_closed
+      end
+    end
+
+    context "for a motion being discussed that hasn't been objected" do
+      before do
+        @motion = Factory(:discussing_motion)
+      end
+
+      it "doesn't update the motion state before 24 hours" do
+        ScheduledMotionUpdate.perform(@motion.id, 'discussing', 23.hours)
+        @motion.reload.should be_discussing
+      end
+
+      it "updates the motion state to 'voting' after 24 hours" do
+        ScheduledMotionUpdate.perform(@motion.id, 'discussing', 24.hours)
+        @motion.reload.should be_voting
+      end
+    end
+
+    context "for a motion being discussed that has been objected" do
+      before do
+        @motion = Factory(:discussing_motion)
+        Factory(:membership).member.object(@motion)
+      end
+
+      it "doesn't update the motion state before 48 hours" do
+        ScheduledMotionUpdate.perform(@motion.id, 'discussing', 47.hours)
+        @motion.reload.should be_discussing
+      end
+
+      it "updates the motion state to 'voting' after 48 hours" do
+        ScheduledMotionUpdate.perform(@motion.id, 'discussing', 48.hours)
+        @motion.reload.should be_voting
+      end
+    end
+
+    context "for a motion being voted" do
+      before do
+        @motion = Factory(:voting_motion)
+      end
+
+      it "doesn't update the motion state before 48 hours" do
+        ScheduledMotionUpdate.perform(@motion.id, 'voting', 47.hours)
+        @motion.reload.should be_voting
+      end
+
+      it "updates the motion state to 'closed' after 48" do
+        ScheduledMotionUpdate.perform(@motion.id, 'voting', 48.hours)
+        @motion.reload.should be_closed
+      end
     end
   end
 end
