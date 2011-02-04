43e2a3b64b9570457325e71577fed18fbd105554
Gabriel Andretta <ohhgabriel@gmail.com>
2011-02-04 15:08:56 -0300

    remove (now) redundant actions

--- app/controllers/motion_events_controller.rb
+++ app/controllers/motion_events_controller.rb
@@ -21,46 +21,4 @@ class MotionEventsController < ApplicationController
       # TODO
     end
   end
-
-  # Create a Seconding Event for a Motion
-  # @option params [Fixnum] :motion_id The id of the motion in question
-  def second
-    @motion = Motion.find(params[:motion_id])
-    if MotionSeconding.do(current_member, @motion)
-      flash[:notice] = "You have successfully seconded the motion."
-    else
-      flash[:alert] =  "Something went wrong when seconding the motion"
-    end
-
-    redirect_to motion_url(@motion)
-  end
-
-  # Create an Objection Event for a Motion
-  # @option params [Fixnum] :motion_id The id of the motion in question
-  def object
-    @motion = Motion.find(params[:motion_id])
-
-    if current_member.object(@motion)
-      flash[:notice] = "You have successfully objected the motion."
-    else
-      flash[:alert] =  "Something went wrong when objecting the motion"
-    end
-
-    redirect_to motion_url(@motion)
-  end
-
-  # Create a Voting Event for a Motion
-  # @option params [Fixnum] :motion_id The id of the motion in question
-  # @option params [Fixnum] :vote The vote cast by the member, can be "aye" or "nay"
-  def vote
-    @motion = Motion.find(params[:motion_id])
-
-    if current_member.vote(@motion, params[:vote] == "aye")
-      flash[:notice] = "You have successfully voted the motion."
-    else
-      flash[:alert] =  "Something went wrong when voting the motion"
-    end
-
-    redirect_to motion_url(@motion)
-  end
 end
