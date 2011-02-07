aad6a727db11fface5f41e9d87911106881e39b7
Gabriel Andretta <ohhgabriel@gmail.com>
2011-02-07 16:49:53 -0300

    remove unused action

--- app/controllers/motion_events_controller.rb
+++ app/controllers/motion_events_controller.rb
@@ -2,14 +2,6 @@ class MotionEventsController < ApplicationController
 
   before_filter :authenticate_member!, :except => [:show]
 
-  # Show an Event for a given Motion
-  # @option params [Fixnum] :motion_id The id of the motion in question
-  # @option params [String] :event_id The id of the event to be viewed
-  def show
-    @motion = Motion.find(params[:motion_id])
-    @event  = @motion.events.where :event_id => params[:event_id]
-  end
-
   def create
     @motion = Motion.find(params[:motion_id])
     @event  = current_member.events.new(params[:event].merge(:motion => @motion))
